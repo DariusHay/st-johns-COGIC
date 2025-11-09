bffea91c2ffaf3fa000002236212d7e7b090ee80
Darius Hay <dariushay@Dariuss-MBP.lan>
2025-11-08 20:22:30 -0500

    added domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/st-johns-COGIC/",  // 👈 must match your GitHub repo name
+  base: "/https://stjohncogiccocoa.org",  // 👈 must match your GitHub repo name
   assetsInclude: ["**/*.PNG", "**/*.JPG", "**/*.JPEG", "**/*.SVG"],
   server: {
     proxy: {
